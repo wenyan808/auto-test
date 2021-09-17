610f11872fd7c1b45a21b8fc90840f752a6142a1
handonglin <handonglin@global-hgroup.com>
2021-09-17 13:49:06 +0800

    refactor get_test_data

--- tool/get_test_data.py
+++ tool/get_test_data.py
@@ -11,6 +11,7 @@ import json
 from pathlib import Path,PurePath
 import pandas as pd
 
+
 # 已废弃从Excel读取数据的方法
 # #根据传入的接口名组装文件路径
 # def file_path(api_name):
@@ -62,7 +63,8 @@ from tool.atp import ATP
 def case_data(priority="P0"):
     #通过反射取回用例文件名作为入参去atp取回对应的测试数据
     caller_frame = inspect.stack()[1]
-    file_name = caller_frame.filename.split('/')[7][:-3]
+    # file_name = caller_frame.filename.split('/')[7][:-3]
+    file_name = Path(caller_frame.filename).stem
     return ATP.get_api_test_data_old(file_name)
 
 
