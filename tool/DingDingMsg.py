b17dc1ca78c2315fa5375a6fbee71e7631f5fc44
handonglin <handonglin@global-hgroup.com>
2021-09-23 18:13:51 +0800

    update dingding msg info

--- tool/DingDingMsg.py
+++ tool/DingDingMsg.py
@@ -5,7 +5,7 @@ class DingDingMsg:
     ding_ding_msg = '''{
         "msgtype": "text",
         "text": {
-        "content": "total: ${total} passed: ${passed} failed: ${failed}  error: ${error}  skipped: ${skipped}  run_time: ${run_time} report url: http://172.18.6.183:8080/jenkins/view/autotest(%E8%87%AA%E5%8A%A8%E5%8C%96%E6%B5%8B%E8%AF%95)/job/auto-test/${build_num}/allure/"
+        "content": "total: ${total} \\n\\n passed: ${passed}  \\n\\nfailed: ${failed}  \\n\\n error: ${error} \\n\\n  skipped: ${skipped}  \\n\\n run_time: ${run_time} \\n\\n report url: http://172.18.6.183:8080/jenkins/view/autotest(%E8%87%AA%E5%8A%A8%E5%8C%96%E6%B5%8B%E8%AF%95)/job/auto-test/${build_num}/allure/"
         }}'''
 
     @classmethod
