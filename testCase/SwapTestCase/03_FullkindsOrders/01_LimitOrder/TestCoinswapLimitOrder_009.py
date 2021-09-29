dd2da5cbc7f6d93c940519a979938b5e1ca7b58c
chenwei <chenwei@global-hgroup.com>
2021-09-29 16:20:35 +0800

    增加用例稳定性

--- testCase/SwapTestCase/03_FullkindsOrders/01_LimitOrder/TestCoinswapLimitOrder_009.py
+++ testCase/SwapTestCase/03_FullkindsOrders/01_LimitOrder/TestCoinswapLimitOrder_009.py
@@ -40,8 +40,9 @@ from common.ContractServiceAPI import t as contract_api
 from common.ContractServiceOrder import t as contract_order
 from common.LinearServiceAPI import t as linear_api
 from common.LinearServiceOrder import t as linear_order
-from common.SwapServiceAPI import t as swap_api
+from common.SwapServiceAPI import t as swap_api, SwapService
 from common.SwapServiceOrder import t as swap_order
+from config.conf import COMMON_ACCESS_KEY, COMMON_SECRET_KEY, URL
 
 from pprint import pprint
 import pytest, allure, random, time
@@ -50,6 +51,7 @@ from tool.atp import ATP
 @allure.epic('业务线')  # 这里填业务线
 @allure.feature('功能')  # 这里填功能
 @allure.story('子功能')  # 这里填子功能，没有的话就把本行注释掉
+@pytest.mark.stable
 class TestCoinswapLimitOrder_009:
 
 	@allure.step('前置条件')
@@ -75,13 +77,13 @@ class TestCoinswapLimitOrder_009:
 	def test_execute(self, contract_code):
 		""" 对手价买入开多卖盘无数据以对手价买入会报对手价不存在 """
 		lever_rate = 5
-
-		self.setup()
+		pprint("得到最近的价格")
 		r = swap_api.swap_history_trade(contract_code=contract_code, size='1')
 		pprint(r)
 		# 得到最近的价格
 		lastprice = r['data'][0]['data'][0]['price']
 		lastprice = round((lastprice * 1.01), 2)
+		pprint("挂一个买单")
 		#挂一个买单
 		r = swap_api.swap_order(contract_code=contract_code,
 											  client_order_id='',
@@ -91,6 +93,7 @@ class TestCoinswapLimitOrder_009:
 											  offset='open',
 											  lever_rate=lever_rate,
 											  order_price_type="limit")
+		pprint(r)
 		time.sleep(3)
 		pprint('\n步骤一:获取盘口(卖)\n')
 		r_trend_req = swap_api.swap_depth(contract_code=contract_code, type="step0")
@@ -103,19 +106,20 @@ class TestCoinswapLimitOrder_009:
 			for each_ask in current_asks:
 				each_price, each_amount = each_ask[0], each_ask[1]
 				total_asks += each_amount
-				highest_price = max(highest_price, each_price)
-			pprint("\n步骤二：用操作账号以当前最高价吃掉(买入)所有卖单\n")
-			# service = swapServiceAPI(URL, COMMON_ACCESS_KEY, COMMON_SECRET_KEY)
-			r = swap_api.swap_order(contract_code=contract_code,
+				# highest_price = max(highest_price, each_price)
+				pprint("\n步骤二：用操作账号以当前最高价吃掉(买入)所有卖单\n")
+				service = SwapService(URL, COMMON_ACCESS_KEY, COMMON_SECRET_KEY)
+				pprint(each_price)
+				r = service.swap_order(contract_code=contract_code,
 										client_order_id='',
-										price=highest_price,
+										price=round((each_price * 1.01), 2),
 										volume=total_asks,
 										direction='buy',
 										offset='open',
 										lever_rate=lever_rate,
 										order_price_type='limit')
-			pprint(r)
-			time.sleep(2)
+				pprint(r)
+				time.sleep(1)
 			pprint("\n步骤三：再次查询盘口，确认是否已吃掉所有卖单\n")
 			r_trend_req_confirm = swap_api.swap_depth(contract_code=contract_code, type="step0")
 			pprint(r_trend_req_confirm)
