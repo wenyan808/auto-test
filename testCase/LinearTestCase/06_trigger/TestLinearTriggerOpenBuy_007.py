3814a5cc9f8c69535372cdfd6d3a3f7c4ed85a39
chenwei <chenwei@global-hgroup.com>
2021-09-30 16:20:43 +0800

    新增计划买入用例

--- testCase/LinearTestCase/06_trigger/TestLinearTriggerOpenBuy_007.py
+++ testCase/LinearTestCase/06_trigger/TestLinearTriggerOpenBuy_007.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """# @Date    : 20210930
-# @Author : 
+# @Author : chenwei
     用例标题
         计划委托买入开多未输入触发价
     前置条件
@@ -31,7 +31,7 @@ from common.SwapServiceOrder import t as swap_order
 
 from pprint import pprint
 import pytest, allure, random, time
-
+from tool.atp import ATP
 
 @allure.epic('业务线')  # 这里填业务线
 @allure.feature('功能')  # 这里填功能
@@ -39,12 +39,25 @@ import pytest, allure, random, time
 class TestLinearTriggerOpenBuy_007:
 
     @allure.step('前置条件')
-    def setup(self):
-        print('''  ''')
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, contract_code):
+        # 撤销当前用户 某个品种所有限价挂单
+        ATP.cancel_all_order(contract_code=contract_code)
+        # 修改当前品种杠杆 默认5倍
+        ATP.switch_level(contract_code=contract_code)
+        # 清除盘口所有卖单
+        ATP.clean_market(contract_code=contract_code, direction='sell')
+        # 清除盘口所有买单
+        ATP.clean_market(contract_code=contract_code, direction='buy')
 
     @allure.title('计划委托买入开多未输入触发价')
     @allure.step('测试执行')
-    def test_execute(self, symbol, symbol_period):
+    def test_execute(self, contract_code):
+        orderPrice = 49800
+        volume = 10
+        direction = 'buy'
+        offset = 'open'
+        leverRate = 5
         with allure.step('1、登录合约交易系统'):
             pass
         with allure.step('2、选择币种BTC，选择杠杆5X，点击开仓-计划按钮'):
@@ -56,7 +69,10 @@ class TestLinearTriggerOpenBuy_007:
         with allure.step('5、输入买入量10张'):
             pass
         with allure.step('6、点击买入开多按钮'):
-            pass
+            r = linear_order.linear_swap_triggerOrder_insert(contract_code=contract_code, trigger_price="",
+                                                             order_price=orderPrice, volume=volume, direction=direction,
+                                                             offset=offset, lever_rate=leverRate)
+            print(r)
 
     @allure.step('恢复环境')
     def teardown(self):
