6096b243dedcc9ad57770797ecf409c3cde74828
handonglin <handonglin@global-hgroup.com>
2021-10-11 17:19:33 +0800

    add 3 ws_req_k_line cases

--- common/SwapServiceWS.py
+++ common/SwapServiceWS.py
@@ -3,29 +3,37 @@
 # @Date    : 2020/10/09
 # @Author  : chenwei
 
-from common.util import sub,api_key_sub
-from config.conf import WSURL,ACCESS_KEY,SECRET_KEY
+from common.util import sub, api_key_sub
+from config.conf import WSURL, ACCESS_KEY, SECRET_KEY
 import uuid
-
-
 
 
 class WebsocketSevice:
 
-    def __init__(self,url,access_key,secret_key):
+    def __init__(self, url, access_key, secret_key):
         self.__url = url
         self.__access_key = access_key
         self.__secret_key = secret_key
 
     # 【通用】订阅 KLine 数据
-    def swap_sub_kline(self,contract_code,period):
+    def swap_sub_kline(self, contract_code, period):
         subs = {
-                "sub": "market.{}.kline.{}".format(contract_code,period),
-                "id": "id1"
-            }
+            "sub": "market.{}.kline.{}".format(contract_code, period),
+            "id": "id1"
+        }
         path = '/swap-ws'
         url = self.__url + path
-        return sub(url,subs)
+        return sub(url, subs)
+
+    def swap_req_kline(self, contract_code, period, From, to):
+        subs = {
+            "req": "market.{}.kline.{}".format(contract_code, period),
+            "from": int(From),
+            "to": int(to)
+        }
+        path = '/swap-ws'
+        url = self.__url + path
+        return sub(url, subs)
 
     # 【通用】订阅 Market Depth 数据
     def swap_sub_depth(self, contract_code, type):
@@ -48,6 +56,4 @@ class WebsocketSevice:
         return sub(url, subs)
 
 
-
-
-t = WebsocketSevice(WSURL,ACCESS_KEY,SECRET_KEY)
+t = WebsocketSevice(WSURL, ACCESS_KEY, SECRET_KEY)
