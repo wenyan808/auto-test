cbb9f473a919f9e457ff012bf764b3af8568d865
ligongxun <ligongxun@global-hgroup.com>
2021-10-12 19:49:55 +0800

    update class name

--- testCase/SwapTestCase/03_FullkindsOrders/02_TriggerOrder/TestSwapTriggerCloseSell_005.py
+++ testCase/SwapTestCase/03_FullkindsOrders/02_TriggerOrder/TestSwapTriggerCloseSell_005.py
@@ -40,9 +40,9 @@ import pytest, allure, random, time
 
 @allure.epic('反向永续')  # 这里填业务线
 @allure.feature('计划委托')  # 这里填功能
-#@allure.story('子功能')  # 这里填子功能，没有的话就把本行注释掉
+@allure.story('子功能')  # 这里填子功能，没有的话就把本行注释掉
 @pytest.mark.stable
-class TestCoinswapTriggerOrder_002:
+class TestSwapTriggerCloseSell_005:
 
     @allure.step('前置条件')
     @pytest.fixture(scope='function', autouse=True)
