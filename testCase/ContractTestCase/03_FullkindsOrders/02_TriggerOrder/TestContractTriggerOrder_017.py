87af796558face419dd455a76752c6b27d5507ec
handonglin <handonglin@global-hgroup.com>
2021-10-13 12:04:48 +0800

    fix @info 'epic' for cases

--- testCase/ContractTestCase/03_FullkindsOrders/02_TriggerOrder/TestContractTriggerOrder_017.py
+++ testCase/ContractTestCase/03_FullkindsOrders/02_TriggerOrder/TestContractTriggerOrder_017.py
@@ -41,7 +41,7 @@ import pytest, allure, random, time
 from config.conf import URL, ACCESS_KEY, SECRET_KEY
 
 
-@allure.epic('业务线')  # 这里填业务线
+@allure.epic('反向交割')  # 这里填业务线
 @allure.feature('功能')  # 这里填功能
 @allure.story('子功能')  # 这里填子功能，没有的话就把本行注释掉
 @pytest.mark.stable
