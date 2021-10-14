4dd4f53430acb5c46904bb06dd2e01c6e6384508
yuhuiqing <yuhuiqing@global-hgroup.com>
2021-10-14 11:46:54 +0800

    正向永续用例

--- testCase/LinearTestCase/07_Noti/TestLinearNoti_001.py
+++ testCase/LinearTestCase/07_Noti/TestLinearNoti_001.py
@@ -18,15 +18,13 @@
 
 from common.LinearServiceAPI import t as linear_api
 from common.LinearServiceWS import t as linear_service_ws
-import json
-from pprint import pprint
 import pytest, allure, random, time
-from tool import atp
+from tool.atp import  ATP
 
 
 @allure.epic('正向永续')  # 这里填业务线
 @allure.feature('WS订阅')  # 这里填功能
-@allure.story('订阅Kline')  # 这里填子功能，没有的话就把本行注释掉
+@allure.story('请求K线(传参from,to)')  # 这里填子功能，没有的话就把本行注释掉
 @pytest.mark.stable
 @allure.tag('Script owner : 余辉青', 'Case owner : 吉龙')
 class TestLinearNoti_001:
@@ -34,59 +32,73 @@ class TestLinearNoti_001:
     @allure.step('前置条件')
     @pytest.fixture(scope='function', autouse=True)
     def setup(self,contract_code,lever_rate,offsetO,directionB,directionS):
+        print("\n自动化步骤："
+              "\n*、先执行两笔成交，更新K线数据；"
+              "\n*、再发送req请求from（now - 1分钟） to(now)kline 1min的请求；"
+              "\n*、验证Kline 1min返回结果；各返回值不为空[]")
         self.lever_rate = lever_rate
         self.contract_code = contract_code
         self.order_price_type = 'limit'
         self.offsetO = offsetO
-        self.currentPrice = atp.ATP.get_current_price()  # 最新价
-        self.lowPrice = round(self.currentPrice * 0.09, 2)  # 买入价
+        self.currentPrice = ATP.get_current_price()  # 最新价
+        self.lowPrice = round(self.currentPrice * 0.99, 2)  # 买入价
         self.highPrice = round(self.currentPrice * 1.01, 2)  # 触发价
         self.directionB = directionB
         self.directionS = directionS
-        print('进行2笔交易，更新Kline数据')
-        linear_api.linear_order(contract_code=self.contract_code, price= self.lowPrice, order_price_type=self.order_price_type,
-                                lever_rate=self.lever_rate,direction=self.directionB, offset=self.offsetO,volume=1)
-
-        linear_api.linear_order(contract_code=self.contract_code, price=self.lowPrice, order_price_type=self.order_price_type,
-                                lever_rate=self.lever_rate,direction=self.directionS, offset=self.offsetO,volume=1)
-        # 等待成交刷新最新价
-        time.sleep(1)
-        linear_api.linear_order(contract_code=self.contract_code, price=self.highPrice,
-                                order_price_type=self.order_price_type,
-                                lever_rate=self.lever_rate, direction=self.directionB, offset=self.offsetO, volume=1)
-
-        linear_api.linear_order(contract_code=self.contract_code, price=self.highPrice,
-                                order_price_type=self.order_price_type,
-                                lever_rate=self.lever_rate, direction=self.directionS, offset=self.offsetO, volume=1)
-        # 等待成交刷新最新价
-        time.sleep(1)
+        ATP.common_user_make_order(price=self.lowPrice, direction='buy')
+        ATP.common_user_make_order(price=self.lowPrice, direction='sell')
+        ATP.common_user_make_order(price=self.highPrice, direction='buy')
+        ATP.common_user_make_order(price=self.highPrice, direction='sell')
 
     @allure.title('请求K线(传参from,to)')
     @allure.step('测试执行')
     def test_execute(self):
         with allure.step('1、订阅Kline获取订阅结果'):
             self.period = '1min'
-            result = linear_service_ws.linear_sub_kline(contract_code=self.contract_code,period=self.period)
-            resultStr = '\nKline返回结果 = ' + str(result)
-            print('\033[1;32;49m%s\033[0m' % resultStr)
-            # 最低价校验
-            if result['tick']['low'] != self.lowPrice:
-                assert False
-            # 最高价校验
-            if result['tick']['high'] != self.highPrice:
-                assert False
-            # 收仓价校验
-            if result['tick']['close'] != self.highPrice:
-                assert False
-            # 成交额, 即sum（每一笔成交张数 * 合约面值 * 成交价格）。 值是买卖双边之和
-            if result['tick']['trade_turnover'] == 0:
-                assert False
-            # 成交量张数。 值是买卖双边之和
-            if result['tick']['vol'] < 4:
-                assert False
-            # 成交笔数。 值是买卖双边之和
-            if result['tick']['count'] < 2:
-                assert False
+            self.toTime = int(time.time())
+            self.fromTime = self.toTime - 60 * 3
+            subs = {
+                "req": "market.{}.kline.{}".format(self.contract_code, self.period),
+                "id": "id1",
+                "from": self.fromTime,
+                "to": self.toTime
+            }
+            tryTimes = 1
+            while True:
+                result = linear_service_ws.linear_sub(subs)
+                resultStr = '\nKline返回结果 = ' + str(result)
+                print('\033[1;32;49m%s\033[0m' % resultStr)
+                # 由于Kline可能更新有点慢，等1秒，再执行一次获取结果；避免失败用例造成死循环；这里重试5次
+                if 'data' in result:
+                    break
+                else:
+                    # 超过5次，跳过循环
+                    if tryTimes > 5:
+                        break
+                    else:
+                        tryTimes = tryTimes + 1
+                        time.sleep(1)
+                        print('k线未返回预期数据，等待1秒，第', tryTimes - 1, '次重试………………')
+            # 请求topic校验
+            assert result['rep'] == "market." + self.contract_code + ".kline." + self.period
+            # 返回的data数量
+            c = 0
+            while c < len(result['data']):
+                # 开仓价校验，不为空
+                assert result['data'][c]['open'] is not None
+                # 收仓价校验
+                assert result['data'][c]['close'] is not None
+                # 最低价校验,不为空
+                assert result['data'][c]['low'] is not None
+                # 最高价校验,不为空
+                assert result['data'][c]['high'] is not None
+                # 币的成交量
+                assert result['data'][c]['amount'] >= 0
+                # 成交量张数。 值是买卖双边之和
+                assert result['data'][c]['vol'] >= 0
+                # 成交笔数。 值是买卖双边之和
+                assert result['data'][c]['count'] >= 0
+                c = c + 1
             pass
 
     @allure.step('恢复环境')
--- testCase/LinearTestCase/07_Noti/TestLinearNoti_004.py
+++ testCase/LinearTestCase/07_Noti/TestLinearNoti_004.py
@@ -16,17 +16,11 @@
         TestLinearNoti_004
 """
 
-from common.ContractServiceAPI import t as contract_api
-from common.ContractServiceOrder import t as contract_order
 from common.LinearServiceAPI import t as linear_api
 from common.LinearServiceOrder import t as linear_order
-from common.SwapServiceAPI import t as swap_api
-from common.SwapServiceOrder import t as swap_order
-
-from pprint import pprint
-import pytest, allure, random, time
 from common.LinearServiceWS import t as linear_service_ws
-
+import pytest, allure, random, time
+from tool.atp import ATP
 
 @allure.epic('正向永续')  # 这里填业务线
 @allure.feature('WS订阅')  # 这里填功能
@@ -35,24 +29,22 @@ from common.LinearServiceWS import t as linear_service_ws
 class TestLinearNoti_004:
 
     @allure.step('前置条件')
-    def setup(self):
-        lever_rate = 5
-        contractCode = 'BTC-USDT'
-        order_price_type = 'limit'
-        offset = 'open'
-        buyPrice = '45000'
-        sellPrice = '50000'
-        buy = 'buy'
-        sell = 'sell'
-        print('进行挂单，更新买卖单数据')
-        linear_api.linear_order(contract_code=contractCode, price=buyPrice, order_price_type=order_price_type,
-                                lever_rate=lever_rate,
-                                direction=buy, offset=offset,
-                                volume=10)
-        linear_api.linear_order(contract_code=contractCode, price=sellPrice, order_price_type=order_price_type,
-                                lever_rate=lever_rate,
-                                direction=sell, offset=offset,
-                                volume=10)
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, contract_code, lever_rate, offsetO, directionB, directionS):
+        self.lever_rate = lever_rate
+        self.contract_code = contract_code
+        self.order_price_type = 'limit'
+        self.offsetO = offsetO
+        self.currentPrice = ATP.get_current_price()  # 最新价
+        self.lowPrice = round(self.currentPrice * 0.99, 2)  # 买入价
+        self.highPrice = round(self.currentPrice * 1.01, 2)  # 触发价
+        self.directionB = directionB
+        self.directionS = directionS
+        n = 1
+        while n<3:
+            ATP.common_user_make_order(price=round(self.currentPrice * (1 - 0.01 * n), 2), direction='buy')
+            ATP.common_user_make_order(price=round(self.currentPrice * (1 + 0.01 * n), 2), direction='sell')
+            n =n +1
         # 等待深度更新
         time.sleep(3)
 
@@ -60,14 +52,30 @@ class TestLinearNoti_004:
     @allure.step('测试执行')
     def test_execute(self):
         with allure.step('WS订阅BBO(单个合约，即传参合约code)，可参考文档：https://docs.huobigroup.com/docs/usdt_swap/v1/cn/#websocket-3'):
-            contractCode = 'BTC-USDT'
-            result = linear_service_ws.linear_sub_bbo(contract_code=contractCode)
-            resultStr = '\nDepth返回结果 = ' + str(result)
-            print('\033[1;32;49m%s\033[0m' % resultStr)
-            if not result['tick']['bid']:
-                assert False
-            if not result['tick']['ask']:
-                assert False
+            subs = {
+                "sub": "market.{}.bbo".format(self.contract_code),
+                "id": "id8"
+            }
+            tryTimes = 1
+            while True:
+                result = linear_service_ws.linear_sub(subs)
+                resultStr = '\n返回结果 = ' + str(result)
+                print('\033[1;32;49m%s\033[0m' % resultStr)
+                # 由于Kline可能更新有点慢，等1秒，再执行一次获取结果；避免失败用例造成死循环；这里重试5次
+                if 'tick' in result and 'bid' in result['tick']:
+                    break
+                else:
+                    # 超过5次，跳过循环
+                    if tryTimes > 5:
+                        break
+                    else:
+                        tryTimes = tryTimes + 1
+                        time.sleep(1)
+                        print('WS未返回预期数据，等待1秒，第', tryTimes - 1, '次重试………………')
+            # 请求topic校验
+            assert result['ch'] == "market." + self.contract_code + ".bbo"
+            assert result['tick']['bid'] is not None, 'bids为空'
+            assert result['tick']['ask'] is not None, 'asks为空'
             pass
 
     @allure.step('恢复环境')
