9dfb3f6730edc5a288d9c0463a3ac2477adee9d9
handonglin <handonglin@global-hgroup.com>
2021-10-14 21:37:30 +0800

    update count logic for dingdingmsg

--- tool/DingDingMsg.py
+++ tool/DingDingMsg.py
@@ -9,7 +9,7 @@ class DingDingMsg:
     ding_ding_msg = '''{
         "msgtype": "text",
         "text": {
-        "content": "env: '${env}' system_type: '${system_type}' test_type: '${test_type}' \\ntotal: ${total} passed: ${passed}  \\nfailed: ${failed}  error: ${error} skipped: ${skipped}  \\nrun_time: ${run_time} \\nreport url: \\nhttp://172.18.6.183:8080/jenkins/view/autotest(%E8%87%AA%E5%8A%A8%E5%8C%96%E6%B5%8B%E8%AF%95)/job/auto-test/${build_num}/allure/"
+        "content": "env: '${env}' system_type: '${system_type}' test_type: '${test_type}' \\ntotal: ${total} passed: ${passed}  \\nfailed: ${failed}  broken: ${broken} skipped: ${skipped}  \\nrun_time: ${run_time} \\nreport url: \\nhttp://172.18.6.183:8080/jenkins/view/autotest(%E8%87%AA%E5%8A%A8%E5%8C%96%E6%B5%8B%E8%AF%95)/job/auto-test/${build_num}/allure/"
         }}'''
 
     @classmethod
