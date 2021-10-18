4a5dd67410dfdc34caaf1b1a473cdad4c9c453b3
ligongxun <ligongxun@global-hgroup.com>
2021-10-18 19:25:31 +0800

    add  73 to 99

--- testCase/LinearTestCase/02_FundsTransfer/TestUSDTSwapTransfer_096.py
+++ testCase/LinearTestCase/02_FundsTransfer/TestUSDTSwapTransfer_096.py
@@ -7,15 +7,15 @@
 所属分组
     资金划转（含母子划转，借贷币划转）
 用例标题
-    子账户逐仓划转到母账户逐仓
+    子账户全仓划转到母账户全仓
 前置条件
     
 步骤/文本
     1、登入U本位合约界面
     2、点击“划转”按钮
     3、币种选择（如：usdt）
-    4、选择“USDT本位永续合约账户-USDT”划转到“USDT本位永续合约账户-BTC/USDT(逐仓)”
-    5、输入划转金额（可转数量<划转金额）
+    4、选择“USDT本位永续合约账户-USDT”划转到“USDT本位永续合约账户-BTC/USDT(全仓)”
+    5、输入划转金额（可转数量<划转金额<全仓账户权益）
     6、点击“确定按钮”
 
 预期结果
@@ -34,17 +34,17 @@ from tool.atp import ATP
 
 @allure.epic('正向永续')  # 这里填业务线
 @allure.feature('资金划转（含母子划转，借贷币划转)母子划转')  # 这里填功能
-@allure.story('子账户逐仓划转到母账户逐仓')  # 这里填子功能，没有的话就把本行注释掉
+@allure.story('子账户全仓划转到母账户全仓')  # 这里填子功能，没有的话就把本行注释掉
 @allure.tag('Script owner : Alex Li', 'Case owner : Alex Li')
 @pytest.mark.stable
-class TestUSDTSwapTransfer_073:
+class TestUSDTSwapTransfer_068:
 
     @allure.step('前置条件')
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, sub_uid):
         print("前置条件  {}".format(sub_uid))
 
-    @allure.title('子账户逐仓划转到母账户逐仓')
+    @allure.title('子账户全仓划转到母账户全仓')
     @allure.step('测试执行')
     def test_execute(self, sub_uid):
         with allure.step('1、登入合约界面'):
@@ -53,16 +53,16 @@ class TestUSDTSwapTransfer_073:
             pass
         with allure.step('3、币种选择（如：BTC）'):
             pass
-        with allure.step('4、选择“USDT本位永续合约账户-USDT”划转到“USDT本位永续合约账户-BTC/USDT(逐仓)”'):
+        with allure.step('4、选择“USDT本位永续合约账户-USDT”划转到“USDT本位永续合约账户-BTC/USDT(全仓)”'):
             pass
-        with allure.step('5、输入划转金额（可转数量<划转金额）'):
+        with allure.step('5、输入划转金额（可转数量<划转金额<全仓账户权益）'):
             pass
         with allure.step('6、点击“确定按钮”'):
 
-            # 子账户逐仓
-            contract_code = 'BTC-USDT'
-            master_account_info = linear_api.linear_sub_account_info(
-                contract_code=contract_code, sub_uid=sub_uid)
+            # 子账户全仓
+            contract_code = 'USDT'
+            master_account_info = linear_api.linear_cross_sub_account_info(
+                margin_account=contract_code, sub_uid=sub_uid)
 
             pprint(master_account_info)
             # 可划转数量
@@ -72,7 +72,7 @@ class TestUSDTSwapTransfer_073:
                     master_account_info['data'][0]['withdraw_available'])
             # 划转金额大于可转数量
             amount = round(withdraw_available+2, 2)
-            res = linear_api.linear_master_sub_transfer(from_margin_account='BTC-USDT', to_margin_account='BTC-USDT',
+            res = linear_api.linear_master_sub_transfer(from_margin_account='USDT', to_margin_account='USDT',
                                                         amount=amount,
                                                         sub_uid=sub_uid,
                                                         type='sub_to_master', asset="USDT")
