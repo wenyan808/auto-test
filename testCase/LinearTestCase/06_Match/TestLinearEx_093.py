4048e67b79631ad95760788d2bd15211ed41d174
ligongxun <ligongxun@global-hgroup.com>
2021-10-28 20:12:31 +0800

    add TestLine Ex

--- testCase/LinearTestCase/06_Match/TestLinearEx_093.py
+++ testCase/LinearTestCase/06_Match/TestLinearEx_093.py
@@ -6,7 +6,7 @@
 所属分组
     合约测试基线用例//03 正向永续//06 撮合//委托单
 用例标题
-    撮合 对手价 买入 平仓     
+    撮合 买入开仓 部分成交单人多笔价格相同的订单   
 前置条件
     
 步骤/文本
@@ -17,8 +17,8 @@
 优先级
     0
 """
-
 import time
+from common.LinearServiceAPI import t as linear_api
 from pprint import pprint
 import pytest
 import allure
@@ -27,51 +27,37 @@ from tool.atp import ATP
 
 @allure.epic('正向永续')  # 这里填业务线
 @allure.feature('撮合//委托单')  # 这里填功能
-@allure.story('撮合 对手价 买入 平仓')  # 这里填子功能，没有的话就把本行注释掉
+@allure.story('撮合 买入开仓 部分成交单人多笔价格相同的订单')  # 这里填子功能，没有的话就把本行注释掉
 @allure.tag('Script owner : Alex Li', 'Case owner : Alex Li')
 @pytest.mark.stable
-class TestLinearEx_007:
+class TestLinearEx_093:
 
     @allure.step('前置条件')
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, contract_code):
         print("前置条件  {}".format(contract_code))
 
-    @allure.title('撮合 对手价 买入 平仓')
+    @allure.title('撮合 买入开仓 部分成交单人多笔价格相同的订单')
     @allure.step('测试执行')
     def test_execute(self, contract_code):
-        with allure.step('1、 撮合 对手价 买入 平仓'):
+        with allure.step('1、 撮合 买入开仓 部分成交单人多笔价格相同的订单'):
             pass
         with allure.step('2、点击“确定按钮”'):
 
-            # 先买入，才能卖出
             current = ATP.get_current_price(contract_code=contract_code)
             offset = 'open'
-            direction = 'sell'
-            order_price_type = "opponent"
+            direction = 'buy'
+            order_price_type = "limit"
 
-            rep_buy = ATP.common_user_make_order(contract_code=contract_code,
-                                                 price=current, volume=10, direction="buy", offset='open')
-            pprint(rep_buy)
+            ATP.common_user_make_order(
+                contract_code=contract_code, volume=10, price=current, direction='sell', offset='open')
+            ATP.common_user_make_order(
+                contract_code=contract_code, volume=10, price=current, direction='sell', offset='open')
+
             res = ATP.current_user_make_order(order_price_type=order_price_type, contract_code=contract_code,
-                                              price=current, volume=10, direction=direction, offset=offset)
+                                              price=current, volume=15, direction=direction, offset=offset)
             pprint(res)
             assert res['status'] == 'ok', "撮合失败！"
-            time.sleep(1)
-            # 买入 平仓
-            rep_sell = ATP.common_user_make_order(contract_code=contract_code,
-                                                  price=current, volume=10, direction="sell", offset='close')
-            pprint(rep_sell)
-            time.sleep(1)
-
-            current1 = ATP.get_current_price(contract_code=contract_code)
-            offset1 = 'close'
-            direction1 = 'buy'
-            res1 = ATP.current_user_make_order(order_price_type=order_price_type, contract_code=contract_code,
-                                               price=current1, volume=10, direction=direction1, offset=offset1)
-            pprint(res1)
-
-            assert res1['status'] == 'ok', "撮合失败！"
 
     @allure.step('恢复环境')
     def teardown(self):
@@ -80,7 +66,6 @@ class TestLinearEx_007:
         # 撤销当前用户 某个品种所有限价挂单
         print(ATP.cancel_all_order())
         print(ATP.make_market_depth())
-        print('\n恢复环境操作结束')
 
 
 if __name__ == '__main__':
