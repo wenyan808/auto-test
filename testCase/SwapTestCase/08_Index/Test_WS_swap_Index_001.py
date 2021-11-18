46392f843541d210cd8e4b78560303323946b428
yuhuiqing <yuhuiqing@global-hgroup.com>
2021-11-18 18:58:01 +0800

    正向永续用例

--- testCase/SwapTestCase/08_Index/Test_WS_swap_Index_001.py
+++ testCase/SwapTestCase/08_Index/Test_WS_swap_Index_001.py
@@ -4,11 +4,8 @@
 # @Author  : yuhuiqing
 from tool.atp import ATP
 import pytest, allure, random, time
-from common.mysqlComm import orderSeq as DB_orderSeq
-from common.SwapServiceAPI import user01 as api_user01
 from common.SwapServiceWS import user01 as ws_user01
 from config.conf import DEFAULT_CONTRACT_CODE
-from common.CommonUtils import retryUtil
 
 @allure.epic('反向永续')
 @allure.feature('指数基差')
@@ -38,18 +35,27 @@ class Test_WS_swap_Index_001:
         with allure.step(''):
             pass
 
-    @pytest.mark.flaky(reruns=3, reruns_delay=1)
+    @pytest.mark.flaky(reruns=1, reruns_delay=1)
     @pytest.mark.parametrize('params', params, ids=ids)
     def test_execute(self, params):
         allure.dynamic.title('指数基差(sub) ' + params['case_name'])
-        with allure.step('执行sub请求'):
+        with allure.step('操作：执行sub请求'):
             subs =  {
                         "sub": "market.{}.{}.{}".format(self.contract_code,params['type'],params['period']),
                         "id": "id1"
                      }
-            result = retryUtil(ws_user01.swap_sub_index,subs,'tick')
+            flag = False
+            # 重试3次未返回预期结果则失败
+            for i in range(1, 4):
+                result = ws_user01.swap_sub_index(subs)
+                if 'tick' in result:
+                    flag = True
+                    break
+                time.sleep(1)
+                print('未返回预期结果，第{}次重试………………………………'.format(i))
+            assert flag
             pass
-        with allure.step('验证响应结果'):
+        with allure.step('验证：返回结果各字段不为空'):
             assert result['tick'] != [], 'data空值'
             # 待校验的字段
             checked_col = ['id', 'open', 'close', 'high', 'low', 'amount', 'vol', 'count']
