d010affb796e14909d6f3453c55e910a0fceb458
yuhuiqing <yuhuiqing@global-hgroup.com>
2021-11-26 15:00:26 +0800

    反向永续用例

--- testCase/SwapTestCase/02_FundsTransfer/TestCoinSwapTransfer_006.py
+++ testCase/SwapTestCase/02_FundsTransfer/TestCoinSwapTransfer_006.py
@@ -1,58 +1,77 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Date    : 2020/7/1
-# @Author  : 张广南
+# @Author  : HuiQing Yu
 
 
-from common.SwapServiceAPI import t as swap_api
+import allure
+import pytest
+import random
+import time
 
-from schema import Schema, And, Or, Regex, SchemaError
-from pprint import pprint
-import pytest, allure, random, time
-from tool.get_test_data import case_data
-
+from common.SwapServiceAPI import user01
+from config.conf import DEFAULT_CONTRACT_CODE,DEFAULT_SYMBOL
+from common.redisComm import redisConf
 
 @allure.epic('反向永续')
-@allure.feature('获取用户的合约账户和持仓信息')
+@allure.feature('资金划转')
+@allure.story('母子划转')
+@allure.tag('Script owner : 张广南', 'Case owner : 叶永刚')
 @pytest.mark.stable
 class TestCoinSwapTransfer_006:
 
-    def setUp(self):
-        print('\n前置条件')
+    @classmethod
+    def setup_class(cls):
+        with allure.step("变量初始化"):
+            cls.contract_code = DEFAULT_CONTRACT_CODE
+            cls.symbol = DEFAULT_SYMBOL
+            cls.redisClient = redisConf('redis6380').instance()
+            pass
+        with allure.step('获取当前母账号与子账号资金信息'):
+            cls.f_account = str(cls.redisClient.hmget('RsT:APO:11538483#'+cls.symbol,'Account:#'+cls.symbol)).split(',')[26]
+            cls.c_account = str(cls.redisClient.hmget('RsT:APO:11539580#'+cls.symbol,'Account:#'+cls.symbol)).split(',')[26]
+            print('母账户初始金额={}，子账户初始金额={}'.format(cls.f_account,cls.c_account))
+            pass
 
+    @classmethod
+    def teardown_class(cls):
+        with allure.step(''):
+            pass
 
-    def test_contract_account_position_info(self, contract_code):
-        subuid = ""
-        amount = "0.19"
-        symbol = contract_code.split("-")[0]
-        expectedresult = (symbol, float(amount))
+    @allure.title('子账户划转到母账号')
+    def test_execute(self, contract_code):
+        with allure.step("操作：执行划转，子 划 母"):
+            amount = random.randint(10,800)
+            result = user01.swap_master_sub_transfer(sub_uid='115395803',contract_code=self.contract_code,amount=amount,type='sub_to_master')
+            pass
+        with allure.step("验证：划转成功还回单号"):
+            assert 'ok' in result['status'] and result['data']['order_id']
+            pass
+        with allure.step("操作：获取当前母子账号资金信息"):
+            f_current_account = str(self.redisClient.hmget('RsT:APO:11538483#'+self.symbol,'Account:#'+self.symbol)).split(',')[26]
+            c_current_account = str(self.redisClient.hmget('RsT:APO:11539580#'+self.symbol,'Account:#'+self.symbol)).split(',')[26]
+            print('母账户当前资金={}，子账户当前资金={}'.format(f_current_account, c_current_account))
+            pass
+        with allure.step("验证：母账号金额更新正确"):
+            assert amount == float(f_current_account) - float(self.f_account)
+            pass
+        with allure.step("验证：子账号金额更新正确"):
+            assert -amount == float(c_current_account) - float(self.c_account)
+            pass
+        with allure.step("验证：存在划转记录"):
+            flag = False
+            for i in range(3):
+                record = user01.swap_master_sub_transfer_record(contract_code=self.contract_code,transfer_type='35',
+                                                                page_size=1,page_index=1,create_date=1)
+                if amount == record['data']['transfer_record'][0]['amount']:
+                    flag=True
+                    break
+                else:
+                    print("验证失败,第{}次重试……".format(i + 1))
+                    time.sleep(1)
+            assert flag,'多次重试，记录金额与实操金额不一致'
+            pass
 
-        self.setUp()
-        r = swap_api.swap_sub_account_info_list(contract_code=contract_code)
-        pprint(r)
-        sublist = r['data']['sub_list']
-        if sublist != []:
-            subuid = sublist[0]['sub_uid']
-
-        r = swap_api.swap_master_sub_transfer(contract_code=contract_code,
-                                              amount=amount,
-                                              sub_uid=subuid,
-                                              type='sub_to_master')
-        pprint(r)
-        time.sleep(2)
-        r2 = swap_api.swap_financial_record(contract_code=contract_code,
-                                            type='35',
-                                            create_date='',
-                                            page_index='',
-                                            page_size='')
-        pprint(r2)
-        financial_record_lastest = r2['data']['financial_record'][0]
-
-        actual = (financial_record_lastest['symbol'], financial_record_lastest['amount'])
-
-        pprint(financial_record_lastest)
-
-        assert actual == expectedresult
 
 
 if __name__ == '__main__':
