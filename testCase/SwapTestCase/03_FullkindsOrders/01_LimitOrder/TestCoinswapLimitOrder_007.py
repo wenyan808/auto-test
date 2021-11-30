676aa3d0e92953dca7d0cbc961f4c0a59d6e9fe8
yuhuiqing <yuhuiqing@global-hgroup.com>
2021-11-30 12:19:25 +0800

    反向永续用例

--- testCase/SwapTestCase/03_FullkindsOrders/01_LimitOrder/TestCoinswapLimitOrder_007.py
+++ testCase/SwapTestCase/03_FullkindsOrders/01_LimitOrder/TestCoinswapLimitOrder_007.py
@@ -25,13 +25,13 @@ class TestCoinswapLimitOrder_007:
     ]
     params = [
         {
-            "case_name": "IOC 买入开多下单后自动撤单测试",
+            "case_name": "FOK 买入开多下单后自动撤单测试",
             "ratio": 1.01,
             "order_price_type": "fok",
             "direction": "buy"
         }, 
         {
-            "case_name": "IOC 卖出开空下单后自动撤单测试",
+            "case_name": "FOK 卖出开空下单后自动撤单测试",
             "ratio": 0.99,
             "order_price_type": "fok",
             "direction": "sell"
@@ -75,7 +75,7 @@ class TestCoinswapLimitOrder_007:
             pass
         with  allure.step('验证：IOC未全部成交，订单被取消'):
             for data in order_info['data']:
-                assert data['status'] == 5
+                assert data['status'] == 7
             pass
         with  allure.step('验证：无资产冻结'):
             account_info = user01.swap_account_info(contract_code=self.contract_code)
--- testCase/SwapTestCase/03_FullkindsOrders/02_TriggerOrder/TestCoinswapTriggerOrder_002.py
+++ testCase/SwapTestCase/03_FullkindsOrders/02_TriggerOrder/TestCoinswapTriggerOrder_002.py
@@ -1,130 +1,129 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @Date    : 20210916
-# @Author :
-"""
-	用例Id
-
-	所属分组
-		计划委托
-	用例标题
-		计划委托正常限价平仓测试
-	前置条件
-
-	类型
-		文本
-	步骤/文本
-		1、登录币本位永续界面
-		2、选择BTC/USD，选择杠杆5X，点击平仓-计划按钮
-		3、输入触发价（如：50000）
-		4、输入买入价（如：49800）
-		5、输入买入量10张
-		6、点击买入平空按钮后弹框确认有结果A
-		7、查看当前委托列表中的计划委托有结果B
-	预期结果
-		A)提示下单成功
-		B)在当前委托-计划委托统计数量+1，列表数量+1,展示合约交易类型，委托类型，倍数，时间，委托数量，委托价信息和下单数值一致
-	标签
-		P0
-	优先级
-		0
-	用例别名
-		TestCoinswapTriggerOrder_002
-"""
-
-from common.SwapServiceAPI import t as swap_api
-from common.util import compare_dict
-
-from pprint import pprint
-import pytest, allure, random, time
+# @Date    : 2020/7/1
+# @Author  : HuiQing Yu
 
 
-@allure.epic('反向永续')  # 这里填业务线
-@allure.feature('计划委托')  # 这里填功能
-#@allure.story('子功能')  # 这里填子功能，没有的话就把本行注释掉
+import allure
+import pytest
+import time
+import random
+
+from config.case_content import epic, features
+from common.SwapServiceAPI import user01
+from config.conf import DEFAULT_CONTRACT_CODE, DEFAULT_SYMBOL
+from common.CommonUtils import currentPrice
+
+
+@allure.epic(epic[1])
+@allure.feature(features[2]['feature'])
+@allure.story(features[2]['story'][2])
+@allure.tag('Script owner : 余辉青', 'Case owner : 邱大伟')
 @pytest.mark.stable
 class TestCoinswapTriggerOrder_002:
+    ids = [
+        "TestCoinswapTriggerOrder_002",
+        "TestCoinswapTriggerOrder_004",
+        "TestCoinswapTriggerOrder_005",
+        "TestCoinswapTriggerOrder_006",
+        "TestCoinswapTriggerOrder_007",
+        "TestCoinswapTriggerOrder_008",
+        "TestCoinswapTriggerOrder_009",
+    ]
+    params = [
+        {
+            "case_name": "计划委托 正常限价平仓测试",
+            "order_price_type": "limit",
+            "direction":"buy",
+            "ratio": 1.01,
+            "trigger_type":"ge"
+        },{
+            "case_name": "计划委托 最优10挡平仓测试",
+            "order_price_type": "optimal_10",
+            "direction": "buy",
+            "ratio": 1.01,
+            "trigger_type":"ge"
+        },{
+            "case_name": "计划委托 最优20挡平仓测试",
+            "order_price_type": "optimal_20",
+            "direction": "buy",
+            "ratio": 1.01,
+            "trigger_type":"ge"
+        },{
+            "case_name": "计划委托 止盈-限价单",
+            "order_price_type": "limit",
+            "direction": "sell",
+            "ratio": 1.01,
+            "trigger_type":"ge"
+        },{
+            "case_name": "计划委托 止盈-最优档",
+            "order_price_type": "optimal_5",
+            "direction": "sell",
+            "ratio": 1.01,
+            "trigger_type":"ge"
+        },{
+            "case_name": "计划委托 止损-限价单",
+            "order_price_type": "limit",
+            "direction": "sell",
+            "ratio": 0.99,
+            "trigger_type":"le"
+        },{
+            "case_name": "计划委托 止损-最优档",
+            "order_price_type": "optimal_5",
+            "direction": "sell",
+            "ratio": 0.99,
+            "trigger_type":"le"
+        }
+    ]
 
-    @allure.step('前置条件')
-    @pytest.fixture(scope='function', autouse=True)
-    def setup(self, contract_code):
-        print("自买自卖产生持仓")
-
-        r = swap_api.swap_history_trade(contract_code=contract_code, size='1')
-        pprint(r)
-        self.price = r['data'][0]['data'][0]['price']
-        self.leverrate = '5'
-
-        swap_api.swap_order(contract_code=contract_code, price=self.price, volume='20', direction='buy',
-                        offset='open', lever_rate=self.leverrate, order_price_type='limit')
-        time.sleep(0.5)
-        swap_api.swap_order(contract_code=contract_code, price=self.price, volume='20', direction='sell',
-                        offset='open', lever_rate=self.leverrate, order_price_type='limit')
-
-    @allure.title('计划委托正常限价平仓测试')
-    @allure.step('测试执行')
-    def test_execute(self, contract_code):
-        self.contract_code = contract_code
-        orderpricetype = 'optimal_10'
-        sltriggerprice = round((self.price * 0.97), 2)
-        slorderprice = round((self.price * 0.98), 2)
-        r = swap_api.swap_trigger_openorders(contract_code=contract_code,
-                                             page_index='',
-                                             page_size='')
-        totalsize1 = r['data']['total_size']
-
-        with allure.step('1、登录币本位永续界面'):
+    @classmethod
+    def setup_class(cls):
+        with allure.step("变量初始化"):
+            cls.contract_code = DEFAULT_CONTRACT_CODE
+            cls.latest_price = currentPrice()
             pass
-        with allure.step('2、选择BTC/USD，选择杠杆5X，点击平仓-计划按钮'):
+        with allure.step("持仓"):
+            user01.swap_order(contract_code=cls.contract_code,price=cls.latest_price,direction='buy',volume=3)
+            user01.swap_order(contract_code=cls.contract_code,price=cls.latest_price,direction='sell',volume=3)
             pass
-        with allure.step('3、输入触发价（如：50000）'):
-            pass
-        with allure.step('4、输入买入价（如：49800）'):
-            pass
-        with allure.step('5、输入买入量10张'):
-            pass
-        with allure.step('6、点击买入平空按钮后弹框确认有结果A'):
-            r = swap_api.swap_trigger_order(contract_code=contract_code,
-                                            trigger_type='le',
-                                            trigger_price=sltriggerprice,
-                                            order_price=slorderprice,
-                                            order_price_type='limit',
-                                            volume='10',
-                                            direction='buy',
-                                            offset='close',
-                                            lever_rate=self.leverrate)
-            pprint(r)
-            self.orderid = r['data']['order_id_str']
-            print(self.orderid)
-            assert r['status'] == 'ok'
-            time.sleep(2)
-            r = swap_api.swap_trigger_openorders(contract_code=contract_code,
-                                                 page_index='',
-                                                 page_size='')
-            totalsize2 = r['data']['total_size']
-            actual_orderinfo = r['data']['orders'][0]
-            pprint(actual_orderinfo)
-        with allure.step('7、查看当前委托列表中的计划委托有结果B'):
-            assert totalsize2 - totalsize1 == 1
-            expectdic = {'contract_code': contract_code,
-                         'order_price': slorderprice,
-                         'order_id': self.orderid,
-                         'trigger_type': 'le',
-                         'trigger_price': sltriggerprice,
-                         'volume': 10.0,
-                         'lever_rate': self.leverrate,
-                         'offset': 'close'
-                         }
-            assert compare_dict(expectdic, actual_orderinfo)
 
-    @allure.step('恢复环境')
-    def teardown(self):
-        print('\n恢复环境操作')
-        if self.orderid:
-            swap_api.swap_trigger_cancel(contract_code=self.contract_code, order_id=self.orderid)
-        r = swap_api.swap_empty_position(contract_code=self.contract_code, price=self.price)
-        print('\n恢复环境操作完毕')
-        return r
+    @classmethod
+    def teardown_class(cls):
+        with allure.step('撤销挂单'):
+            user01.swap_trigger_cancelall(contract_code=cls.contract_code)  # 避免用例失败未能撤销订单
+            pass
+
+    @pytest.mark.parametrize('params', params, ids=ids)
+    def test_execute(self, params):
+        allure.dynamic.title(params['case_name'])
+        with allure.step('操作：下单'):
+            order_reps = user01.swap_trigger_order(contract_code=self.contract_code, trigger_type=params['trigger_type'],
+                                                trigger_price=round(self.latest_price * params['ratio'], 2),volume=1,
+                                                order_price=round(self.latest_price * params['ratio'],2),direction='buy',
+                                                offset='close',order_price_type=params['order_price_type'] )
+
+            pass
+        with allure.step('验证：下单成功'):
+            orderId = order_reps['data']['order_id']
+            assert 'ok' in order_reps['status'] and orderId
+            pass
+        with allure.step('验证：订单信息与下单数据一致'):
+            time.sleep(1)  # 等待数据更新
+            orders = user01.swap_trigger_openorders(contract_code=self.contract_code,page_size=1,page_index=1)['data']['orders']
+            assert orders[0]['order_id'] == orderId,'订单校验未存在'
+            assert self.contract_code in orders[0]['contract_code'],'合约校验失败'
+            assert orders[0]['volume'] == 1,'合约数量校验失败'
+            assert orders[0]['lever_rate'] == 5,'合约倍数校验失败'
+            assert orders[0]['trigger_price'] == round(self.latest_price * params['ratio'], 2),'触发价格校验失败'
+            if "optimal" in params['order_price_type']:
+                assert orders[0]['order_price'] == 0E-18,'订单价格校验失败'
+            else:
+                assert orders[0]['order_price'] == round(self.latest_price * params['ratio'], 2), '订单价格校验失败'
+            assert 'close' in orders[0]['offset'],'订单 平|开 校验失败'
+            assert 'buy' in orders[0]['direction'],'订单 买|卖 校验失败'
+            assert params['order_price_type'] in orders[0]['order_price_type'],'订单类型校验失败'
+            pass
+
 
 if __name__ == '__main__':
     pytest.main()
