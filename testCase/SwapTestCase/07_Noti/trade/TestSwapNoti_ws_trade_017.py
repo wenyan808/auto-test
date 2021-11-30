e4a075320be27ec5ae2d607a070ba541c8811486
yuhuiqing <yuhuiqing@global-hgroup.com>
2021-11-30 15:36:53 +0800

    反向永续用例

--- testCase/SwapTestCase/07_Noti/trade/TestSwapNoti_ws_trade_017.py
+++ testCase/SwapTestCase/07_Noti/trade/TestSwapNoti_ws_trade_017.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Date    : 2021/11/10 4:03 下午
-# @Author  : yuhuiqing
-import pytest, allure, random, time
-from common.SwapServiceWS import user01
-from common.SwapServiceAPI import user01 as api_user01
-from config.conf import DEFAULT_CONTRACT_CODE
-from common.CommonUtils import currentPrice
+# @Author  : HuiQing Yu
+import allure
+import pytest
 
-@allure.epic('反向永续')
-@allure.feature('行情')
-@allure.story('成交')
+from common.SwapServiceWS import user01
+from config.case_content import epic, features
+from config.conf import DEFAULT_CONTRACT_CODE
+
+
+@allure.epic(epic[1])
+@allure.feature(features[6]['feature'])
+@allure.story(features[6]['story'][5])
 @allure.tag('Script owner : 余辉青', 'Case owner : 吉龙')
 @pytest.mark.stable
 class TestSwapNoti_ws_trade_017:
