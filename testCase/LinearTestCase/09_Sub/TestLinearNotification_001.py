f1a81ebc65911ea687df41e7d5c2d2c7087c378a
zhangguangnan <zhangguangnan@global-hgroup.com>
2021-12-01 17:12:51 +0800

    临时去掉sub用例

--- testCase/LinearTestCase/09_Sub/TestLinearNotification_001.py
+++ testCase/LinearTestCase/09_Sub/TestLinearNotification_001.py
@@ -30,7 +30,7 @@ from common.LinearServiceWS import t as linear_service_ws
 @allure.feature('订单推送')  # 这里填功能
 @allure.story('订阅逐仓持仓变动（无持仓变动）')  # 这里填子功能，没有的话就把本行注释掉
 @allure.tag('Script owner : Alex Li', 'Case owner : 柳攀峰')
-@pytest.mark.stable
+# @pytest.mark.stable
 class TestLinearNotification_001:
 
     @allure.step('前置条件:非colo域名')
@@ -40,12 +40,12 @@ class TestLinearNotification_001:
 
     @allure.title('订阅逐仓持仓变动（无持仓变动）')
     @allure.step('测试执行')
-    def test_execute(self):
-        contract_code = 'BSV-USDT'
+    def test_execute(self, contract_code):
+        # contract_code = 'BSV-USDT'
         with allure.step('1、 订阅逐仓持仓变动（无持仓变动）'):
             sub = {
                 "op": "sub",
-                "cid": 11538447,
+                "cid": '11538447',
                 "topic": "positions.{}".format(contract_code)
             }
             result = linear_service_ws.linear_notification(sub)
