17fd3796cf7a4836f7f5faba84f410d3f777733c
ligongxun <ligongxun@global-hgroup.com>
2021-12-17 11:53:00 +0800

    update mysqlComm

--- common/mysqlComm.py
+++ common/mysqlComm.py
@@ -5,58 +5,26 @@
 
 import logging
 import traceback
-
 import pymysql
-
-from config.conf import MYSQL_CONF, MYSQL_CONTRACT_CONF
+from config.conf import MYSQL_CONF
 
 
 class mysqlComm(object):
 
-    def __init__(self, db=None, biztype=None):
-        if db:
-            conf = eval(MYSQL_CONF)
-            self.__host = conf[db]['host']
-            self.__port = conf[db]['port']
-            self.__userName = conf[db]['userName']
-            self.__password = conf[db]['passwd']
-            self.__dbName = conf[db]['dbName']
-            try:
-                self.__db = pymysql.connect(host=self.__host, port=self.__port, user=self.__userName,
-                                            password=self.__password, database=self.__dbName)
-            except Exception as e:
-                logging.warning('pymysql.connect Fail')
-                logging.warning(e)
-        elif(biztype and biztype == 'contract'):
-            contract_conf = eval(MYSQL_CONTRACT_CONF)
-            print(str(contract_conf))
-            try:
-                self.__contract_conn = pymysql.connect(
-                    host=contract_conf['host'], port=contract_conf['port'], user=contract_conf['userName'], password=contract_conf['passwd'])
-            except Exception as e:
-                logging.warning('MYSQL_CONTRACT.connect Fail')
-                logging.warning(e)
-
-    def execute(self, sqlStr):
+    def __init__(self):
+        conf = eval(MYSQL_CONF)
         try:
-            self.__db.ping(reconnect=True)
-            cursor = self.__db.cursor()
-            cursor.execute(sqlStr)
-            self.__db.commit()
-            data = cursor.fetchall()
-            print('执行sql = ' + str(sqlStr))
-            print('执行结果 = ' + str(data))
-            return data
+            print("MYSQL CONN CONSTRUCTOR")
+            self.__contract_conn = pymysql.connect(
+                host=conf['host'], port=conf['port'], user=conf['userName'], password=conf['passwd'])
         except Exception as e:
-            print(traceback.print_exc())
-            print('SQL执行异常，操作回滚={}', str(e))
-            self.__db.rollback()
-            self.__db.close()
+            logging.warning('CONNECT MYSQL HOST FAILED')
+            logging.warning(e)
 
-    def contract_selectdb_execute(self, db, sqlStr):
-        print('数据库Schema：{}，执行sql ={}'.format(db, sqlStr))
+    def selectdb_execute(self, dbSchema, sqlStr):
+        print('数据库Schema:{}，执行sql ={}'.format(dbSchema, sqlStr))
         try:
-            self.__contract_conn.select_db(db)
+            self.__contract_conn.select_db(dbSchema)
             cursor = self.__contract_conn.cursor(
                 cursor=pymysql.cursors.DictCursor)
             cursor.execute(sqlStr)
@@ -69,19 +37,12 @@ class mysqlComm(object):
             print('SQL执行异常，操作回滚={}', str(e))
             self.__contract_conn.rollback()
             self.__contract_conn.close()
+        finally:
+            cursor.close()
 
-    def dictCursor(self, sqlStr):
+    def __del__(self):
         try:
-            self.__db.ping(reconnect=True)
-            cursor = self.__db.cursor(cursor=pymysql.cursors.DictCursor)
-            cursor.execute(sqlStr)
-            self.__db.commit()
-            data = cursor.fetchall()
-            print(f'执行sql = {sqlStr}')
-            print(f'执行结果 = {data}')
-            return data
+            print("MYSQL CONN DESTRUCTOR")
+            self.__contract_conn.close()
         except Exception as e:
-            print(traceback.print_exc())
-            print(f'SQL执行异常，操作回滚={e}')
-            self.__db.rollback()
-            self.__db.close()
+            print(e)
