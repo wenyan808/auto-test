8d66b4e9516f96f0d8e89bb75191bafea69116c2
ligongxun <ligongxun@global-hgroup.com>
2021-12-24 11:15:54 +0800

    update 8 mgt cases

--- testCase/ContractTestCase/05_MGT/01_Account/TestContractAccountCapticalBatch_008.py
+++ testCase/ContractTestCase/05_MGT/01_Account/TestContractAccountCapticalBatch_008.py
@@ -112,11 +112,8 @@ class TestContractAccountCapticalBatch_008:
         with allure.step('接口正常返回,各流水类型数据不为0'):
             assert len(self.__data['daily']) > 0
         with allure.step('查询各流水类型的DB数据,接口数据与DB数据进行对比'):
-            contract_conn = mysqlComm()
-            symbol = 'btc'
             # money_type 5:开仓手续费-taker,6:开仓手续费-maker,7:平仓手续费-taker,8:平仓手续费-maker,11:平账账户,14:币币转入,15:币币转出,20:平账,21:借贷转运营,22:运营转借贷,23:手续费转运营,24:注入到爆仓,25:从爆仓提取,26:给用户赠币-赔偿,27:扣减用户资产-惩戒,28:活动奖励,29:返利
             # userType 用户类型 1普通用户，2爆仓用户，3应付外债，4交易手续费，5交割手续费，9运营活动，11是平台资产 12是应付用户 13是平账账户
-            platform_capital_dic = None  # 平台资产
             # 验证如下:userType:平台资产=负债+所有者权益
             A = 0  # 资产
             S = 0  # 负债
@@ -129,8 +126,11 @@ class TestContractAccountCapticalBatch_008:
                     A += Decimal(data[param['money_type']])
                 elif data['userType'] == 4 or data['userType'] == 5 or data['userType'] == 9:
                     O += Decimal(data[param['money_type']])
-
-            assert A == S+O, '{},横向校验失败'.format(param['case_title'])
+            # 横向对账：平账和流水当期发生额不用比较，跳过
+            if(param['id'] == 'TestContractAccountCapticalBatch_024' or param['id'] == 'TestContractAccountCapticalBatch_025'):
+                pass
+            else:
+                assert A == S+O, '{},横向校验失败'.format(param['case_title'])
 
     @allure.step('恢复环境')
     def teardown(self):
