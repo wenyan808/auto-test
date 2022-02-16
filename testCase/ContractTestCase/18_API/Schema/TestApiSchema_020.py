e5981d1bdd322f553214a08b5d5d9ecd3975e71e
zhangguangnan <zhangguangnan@global-hgroup.com>
2022-02-16 20:23:14 +0800

    修改反向交割的脚本错误schema-17~21

--- testCase/ContractTestCase/18_API/Schema/TestApiSchema_020.py
+++ testCase/ContractTestCase/18_API/Schema/TestApiSchema_020.py
@@ -17,15 +17,13 @@
 优先级
     1
 """
-
+from pprint import pprint
 
 import allure
-import common.util
 import pytest
-from common.ContractServiceAPI import common_user_contract_service_api as common_contract_api
-from common.ContractServiceAPI import t as contract_api
 from schema import Or, Schema
-from tool.atp import ATP
+
+from common.ContractServiceAPI import t as contract_api
 
 
 @allure.epic("反向交割")  # 这里填业务线
@@ -39,50 +37,33 @@ class TestApiSchema_020:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, symbol):
         print("前置条件 {}".format(symbol))
-        print(ATP.make_market_depth())
 
     @allure.title("批量获取子账户资产信息")
     @allure.step("测试执行")
-    def test_execute(self, sub_uid):
+    def test_execute(self, symbol):
         with allure.step("1、调用接口：api/v1/contract_sub_account_info_list"):
             pass
         with allure.step("2、接口返回的json格式、字段名、字段值正确"):
             # 构造持仓量
-            price = ATP.get_current_price()
-            common_contract_api.contract_order(
-                symbol="BTC", contract_type="this_week", price=price, volume=1, direction="buy", offset="open")
-            contract_api.contract_order(
-                symbol="BTC", contract_type="this_week", price=price, volume=1, direction="sell", offset="open")
-            res = contract_api.contract_sub_account_info_list(symbol="BTC")
-            print(res)
-            if res["status"] != "error":
-                schema = {
-                    "status": "ok",
-                    "data": {
-                        "total_page": int,
-                        "current_page": int,
-                        "total_size": int,
-                        "sub_list": [
-                         {
-                             "sub_uid": int,
-                             "account_info_list": [
-                                 {
-                                     "symbol": str,
-                                     "margin_balance": Or(float, int, None),
-                                     "liquidation_price": Or(float, int, None),
-                                     "risk_rate": Or(float, int, None)
-                                 }
-                             ]
-                         }
-                        ]
-                    }, 'ts': int}
-                Schema(schema).validate(res)
+            res = contract_api.contract_sub_account_info_list(symbol=symbol)
+            pprint(res)
+
+            schema = {'data': {'current_page': int,
+                               'sub_list': [{'account_info_list': [{'liquidation_price': Or(float, int, None),
+                                                                    'margin_balance': Or(float, int, None),
+                                                                    'risk_rate': Or(float, int, None),
+                                                                    'symbol': symbol}],
+                                             'sub_uid': int}],
+                               'total_page': int,
+                               'total_size': int},
+                      'status': 'ok',
+                      'ts': int}
+
+            Schema(schema).validate(res)
 
     @allure.step("恢复环境")
     def teardown(self):
         print("\n恢复环境操作")
-        print(ATP.clean_market())
-        print(ATP.cancel_all_order())
 
 
 if __name__ == "__main__":
