dc716b8ae3ceaa6a7e5a0afd126f934424da33e9
zhangguangnan <zhangguangnan@global-hgroup.com>
2022-02-16 18:35:50 +0800

    修改反向交割的脚本错误schema-14~16

--- testCase/ContractTestCase/18_API/Schema/TestApiSchema_016.py
+++ testCase/ContractTestCase/18_API/Schema/TestApiSchema_016.py
@@ -17,15 +17,13 @@
 优先级
     1
 """
-
+from pprint import pprint
 
 import allure
-import common.util
 import pytest
-from common.ContractServiceAPI import common_user_contract_service_api as common_contract_api
+from schema import Schema
+
 from common.ContractServiceAPI import t as contract_api
-from schema import Or, Schema
-from tool.atp import ATP
 
 
 @allure.epic('反向交割')  # 这里填业务线
@@ -39,47 +37,32 @@ class TestApiSchema_016:
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, symbol):
         print("前置条件 {}".format(symbol))
-        print(ATP.make_market_depth())
 
     @allure.title('查询系统状态')
     @allure.step('测试执行')
-    def test_execute(self):
+    def test_execute(self, symbol):
         with allure.step('1、调用接口：/api/v1/contract_api_state'):
             pass
         with allure.step('2、接口返回的json格式、字段名、字段值正确'):
             # 构造持仓量
-            price = ATP.get_current_price()
-            common_contract_api.contract_order(
-                symbol="BTC", contract_type="this_week", price=price, volume=1, direction="buy", offset="open")
-            contract_api.contract_order(
-                symbol="BTC", contract_type="this_week", price=price, volume=1, direction="sell", offset="open")
-            res = contract_api.contract_api_state(
-                symbol='BTC')
-            print(res)
-            if res["status"] != 'error':
-                schema = {
-                    "status": "ok",
-                    "data": [
-                        {
-                            "symbol": str,
-                            "open": int,
-                            "close": int,
-                            "cancel": int,
-                            "transfer_in": int,
-                            "transfer_out": int,
-                            "master_transfer_sub": int,
-                            "sub_transfer_master": int
-                        }
-                    ],
-                    "ts": int
-                }
-                Schema(schema).validate(res)
+            res = contract_api.contract_api_state(symbol=symbol)
+            pprint(res)
+
+            schema = {"data": [{"symbol": str,
+                                "open": int,
+                                "close": int,
+                                "cancel": int,
+                                "transfer_in": int,
+                                "transfer_out": int,
+                                "master_transfer_sub": int,
+                                "sub_transfer_master": int}],
+                      "status": "ok",
+                      "ts": int}
+            Schema(schema).validate(res)
 
     @allure.step('恢复环境')
     def teardown(self):
         print('\n恢复环境操作')
-        print(ATP.clean_market())
-        print(ATP.cancel_all_order())
 
 
 if __name__ == '__main__':
