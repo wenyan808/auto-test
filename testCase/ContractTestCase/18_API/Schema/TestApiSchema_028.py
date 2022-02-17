26fdf9be0965c1e9504bd2f70727371f16a5fdff
zhangguangnan <zhangguangnan@global-hgroup.com>
2022-02-17 20:49:08 +0800

    修改反向交割的脚本错误schema-26~30

--- testCase/ContractTestCase/18_API/Schema/TestApiSchema_028.py
+++ testCase/ContractTestCase/18_API/Schema/TestApiSchema_028.py
@@ -17,15 +17,13 @@
 优先级
     1
 """
-
+from pprint import pprint
 
 import allure
-import common.util
 import pytest
-from common.ContractServiceAPI import common_user_contract_service_api as common_contract_api
+from schema import Schema
+
 from common.ContractServiceAPI import t as contract_api
-from schema import Or, Schema
-from tool.atp import ATP
 
 
 @allure.epic('反向交割')  # 这里填业务线
@@ -39,45 +37,32 @@ class TestApiSchema_028:
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, symbol):
         print("前置条件 {}".format(symbol))
-        print(ATP.make_market_depth())
 
     @allure.title('获取用户的合约手续费费率')
     @allure.step('测试执行')
-    def test_execute(self, sub_uid):
+    def test_execute(self, symbol):
         with allure.step('1、调用接口：api/v1/contract_fee'):
             pass
         with allure.step('2、接口返回的json格式、字段名、字段值正确'):
             # 构造持仓量
-            price = ATP.get_current_price()
-            common_contract_api.contract_order(
-                symbol="BTC", contract_type="this_week", price=price, volume=1, direction="buy", offset="open")
-            contract_api.contract_order(
-                symbol="BTC", contract_type="this_week", price=price, volume=1, direction="sell", offset="open")
-            res = contract_api.contract_fee(symbol="BTC")
-            print(res)
-            if res["status"] != "error":
-                schema = {
-                    "status": "ok",
-                    "data": [
-                        {
-                            "symbol": "BTC",
-                            "open_maker_fee": str,
-                            "open_taker_fee": str,
-                            "close_maker_fee": str,
-                            "close_taker_fee": str,
-                            "delivery_fee": str,
-                            "fee_asset": str
-                        }
-                    ],
-                    "ts": int
-                }
-                Schema(schema).validate(res)
+
+            res = contract_api.contract_fee(symbol=symbol)
+            pprint(res)
+
+            schema = {"data": [{"symbol": symbol,
+                                "open_maker_fee": str,
+                                "open_taker_fee": str,
+                                "close_maker_fee": str,
+                                "close_taker_fee": str,
+                                "delivery_fee": str,
+                                "fee_asset": str}],
+                      "status": "ok",
+                      "ts": int}
+            Schema(schema).validate(res)
 
     @allure.step('恢复环境')
     def teardown(self):
         print('\n恢复环境操作')
-        print(ATP.clean_market())
-        print(ATP.cancel_all_order())
 
 
 if __name__ == '__main__':
--- testCase/ContractTestCase/18_API/Schema/TestApiSchema_029.py
+++ testCase/ContractTestCase/18_API/Schema/TestApiSchema_029.py
@@ -17,15 +17,13 @@
 优先级
     1
 """
-
+from pprint import pprint
 
 import allure
-import common.util
 import pytest
-from common.ContractServiceAPI import common_user_contract_service_api as common_contract_api
-from common.ContractServiceAPI import t as contract_api
 from schema import Or, Schema
-from tool.atp import ATP
+
+from common.ContractServiceAPI import t as contract_api
 
 
 @allure.epic('反向交割')  # 这里填业务线
@@ -39,47 +37,34 @@ class TestApiSchema_029:
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, symbol):
         print("前置条件 {}".format(symbol))
-        print(ATP.make_market_depth())
 
     @allure.title('获取用户的合约划转限制')
     @allure.step('测试执行')
-    def test_execute(self, sub_uid):
+    def test_execute(self, symbol):
         with allure.step('1、调用接口：api/v1/contract_transfer_limit'):
             pass
         with allure.step('2、接口返回的json格式、字段名、字段值正确'):
             # 构造持仓量
-            price = ATP.get_current_price()
-            common_contract_api.contract_order(
-                symbol="BTC", contract_type="this_week", price=price, volume=1, direction="buy", offset="open")
-            contract_api.contract_order(
-                symbol="BTC", contract_type="this_week", price=price, volume=1, direction="sell", offset="open")
-            res = contract_api.contract_transfer_limit(symbol="BTC")
-            print(res)
-            if res["status"] != "error":
-                schema = {
-                    "status": "ok",
-                    "data": [
-                        {
-                            "symbol": "BTC",
-                            "transfer_in_max_each": Or(float, int, None),
-                            "transfer_in_min_each": Or(float, int, None),
-                            "transfer_out_max_each": Or(float, int, None),
-                            "transfer_out_min_each": Or(float, int, None),
-                            "transfer_in_max_daily": Or(float, int, None),
-                            "transfer_out_max_daily": Or(float, int, None),
-                            "net_transfer_in_max_daily": Or(float, int, None),
-                            "net_transfer_out_max_daily": Or(float, int, None),
-                        }
-                    ],
-                    "ts": int
-                }
-                Schema(schema).validate(res)
+
+            res = contract_api.contract_transfer_limit(symbol=symbol)
+            pprint(res)
+
+            schema = {"data": [{"symbol": symbol,
+                                "transfer_in_max_each": Or(float, int, None),
+                                "transfer_in_min_each": Or(float, int, None),
+                                "transfer_out_max_each": Or(float, int, None),
+                                "transfer_out_min_each": Or(float, int, None),
+                                "transfer_in_max_daily": Or(float, int, None),
+                                "transfer_out_max_daily": Or(float, int, None),
+                                "net_transfer_in_max_daily": Or(float, int, None),
+                                "net_transfer_out_max_daily": Or(float, int, None)}],
+                      "status": "ok",
+                      "ts": int}
+            Schema(schema).validate(res)
 
     @allure.step('恢复环境')
     def teardown(self):
         print('\n恢复环境操作')
-        print(ATP.clean_market())
-        print(ATP.cancel_all_order())
 
 
 if __name__ == '__main__':
