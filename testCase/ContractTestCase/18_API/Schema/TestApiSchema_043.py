6c56fc06b8e86ee4c3f73861afd6d810e7d09882
zhangguangnan <zhangguangnan@global-hgroup.com>
2022-02-18 21:53:58 +0800

    修改反向交割的脚本错误schema-43

--- testCase/ContractTestCase/18_API/Schema/TestApiSchema_043.py
+++ testCase/ContractTestCase/18_API/Schema/TestApiSchema_043.py
@@ -17,13 +17,13 @@
 优先级
     1
 """
-
+from pprint import pprint
 
 import allure
 import pytest
-from common.ContractServiceAPI import common_user_contract_service_api as common_contract_api
-from common.ContractServiceAPI import t as contract_api
 from schema import Or, Schema
+
+from common.ContractServiceAPI import t as contract_api
 from tool.atp import ATP
 
 
@@ -38,74 +38,67 @@ class TestApiSchema_043:
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, symbol):
         print("前置条件 {}".format(symbol))
-        print(ATP.make_market_depth())
-        print(ATP.cancel_all_order())
-        # 切回持仓倍数
-        res = contract_api.contract_switch_lever_rate(
-            symbol="BTC", lever_rate=5)
-        print(res)
+        print(ATP.clean_market())
 
     @allure.title('获取用户的合约当前未成交委托')
     @allure.step('测试执行')
-    def test_execute(self, sub_uid):
+    def test_execute(self, symbol):
         with allure.step('1、调用接口：api/v1/contract_openorders'):
             pass
         with allure.step('2、接口返回的json格式、字段名、字段值正确'):
             # 构造持仓量
-            price = ATP.get_current_price()
-            res_sell = contract_api.contract_order(
-                symbol="BTC", contract_type="this_week", price=price+10, volume=1, direction="sell", offset="open")
-            print(res_sell)
-            res = contract_api.contract_openorders(symbol="BTC")
-            print(res)
-            if res["status"] != "error":
-                schema = {
-                    "status": "ok",
-                    "data": {
-                        "orders": [
-                            {
-                                "symbol": str,
-                                "contract_code": str,
-                                "contract_type": str,
-                                "volume": Or(float, int),
-                                "price": Or(float, int),
-                                "order_price_type": str,
-                                "order_type": int,
-                                "direction": str,
-                                "offset": str,
-                                "lever_rate": int,
-                                "order_id": int,
-                                "client_order_id": Or(float, int, None),
-                                "created_at": int,
-                                "trade_volume": Or(float, int),
-                                "trade_turnover": Or(float, int),
-                                "fee": Or(float, int),
-                                "trade_avg_price": Or(float, int, None),
-                                "margin_frozen": Or(float, int),
-                                "profit": Or(float, int),
-                                "status": Or(float, int),
-                                "order_source": str,
-                                "order_id_str": str,
-                                "fee_asset": str,
-                                "liquidation_type": Or(float, int, None),
-                                "canceled_at": Or(float, int, None),
-                                "is_tpsl": int,
-                                "update_time": int,
-                                "real_profit": Or(float, int)
-                            }
-                        ],
-                        "total_page": int,
-                        "current_page": int,
-                        "total_size": int
-                    },
-                    "ts": int
-                }
-                Schema(schema).validate(res)
+            price = ATP.get_adjust_price()
+            contract_api.contract_order(
+                symbol=symbol, contract_type="this_week", price=price, volume=1, direction="sell", offset="open")
+            res = contract_api.contract_openorders(symbol=symbol)
+            pprint(res)
+
+            schema = {
+                "status": "ok",
+                "data": {
+                    "orders": [
+                        {
+                            "symbol": str,
+                            "contract_code": str,
+                            "contract_type": str,
+                            "volume": Or(float, int),
+                            "price": Or(float, int),
+                            "order_price_type": str,
+                            "order_type": int,
+                            "direction": str,
+                            "offset": str,
+                            "lever_rate": int,
+                            "order_id": int,
+                            "client_order_id": Or(float, int, None),
+                            "created_at": int,
+                            "trade_volume": Or(float, int),
+                            "trade_turnover": Or(float, int),
+                            "fee": Or(float, int),
+                            "trade_avg_price": Or(float, int, None),
+                            "margin_frozen": Or(float, int),
+                            "profit": Or(float, int),
+                            "status": Or(float, int),
+                            "order_source": str,
+                            "order_id_str": str,
+                            "fee_asset": str,
+                            "liquidation_type": Or(float, int, None),
+                            "canceled_at": Or(float, int, None),
+                            "is_tpsl": int,
+                            "update_time": int,
+                            "real_profit": Or(float, int)
+                        }
+                    ],
+                    "total_page": int,
+                    "current_page": int,
+                    "total_size": int
+                },
+                "ts": int
+            }
+            Schema(schema).validate(res)
 
     @allure.step('恢复环境')
     def teardown(self):
         print('\n恢复环境操作')
-        print(ATP.clean_market())
         print(ATP.cancel_all_order())
 
 
