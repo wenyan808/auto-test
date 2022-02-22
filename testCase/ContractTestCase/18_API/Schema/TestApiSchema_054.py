3ee7a0425094034ffd255a52190ce8525290ba8c
zhangguangnan <zhangguangnan@global-hgroup.com>
2022-02-22 15:45:00 +0800

    修改反向交割的脚本错误schema-53~56

--- testCase/ContractTestCase/18_API/Schema/TestApiSchema_054.py
+++ testCase/ContractTestCase/18_API/Schema/TestApiSchema_054.py
@@ -17,14 +17,15 @@
 优先级
     1
 """
-
+import time
+from pprint import pprint
 
 import allure
-import common.util
 import pytest
-from common.ContractServiceAPI import common_user_contract_service_api as common_contract_api
+from schema import Schema
+
 from common.ContractServiceAPI import t as contract_api
-from schema import Or, Schema
+from common.util import get_contract_type
 from tool.atp import ATP
 
 
@@ -39,51 +40,55 @@ class TestApiSchema_054:
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, symbol):
         print("前置条件 {}".format(symbol))
-        print(ATP.make_market_depth())
-        # 切回持仓倍数
-        res = contract_api.contract_switch_lever_rate(
-            symbol="BTC", lever_rate=5)
-        print(res)
 
     @allure.title('对仓位设置止盈止损订单')
     @allure.step('测试执行')
-    def test_execute(self, sub_uid):
+    def test_execute(self, symbol, symbol_period):
         with allure.step('1、调用接口：api/v1/contract_tpsl_order'):
             pass
         with allure.step('2、接口返回的json格式、字段名、字段值正确'):
             # 构造持仓量
-            price = ATP.get_current_price()
-            common_contract_api.contract_order(
-                symbol="BTC", contract_type="this_week", price=price, volume=5, direction="sell", offset="open")
-            res_sell = contract_api.contract_trigger_order(
-                symbol="BTC", contract_type="this_week", trigger_type="ge", trigger_price=price, order_price=price+1, volume=1, direction="buy", offset="open", lever_rate=5)
-            print(res_sell)
-            res = contract_api.contract_tpsl_order(
-                symbol="BTC", contract_type="this_week", direction="buy", volume=1, tp_trigger_price=price-1,
-                tp_order_price_type="optimal_5", sl_trigger_price=price, sl_order_price=price, sl_order_price_type="optimal_5")
-            print(res)
-            if res["status"] != "error":
-                schema = {
-                    "status": "ok",
-                    "data": {
-                        "tp_order": {
-                            "order_id": int,
-                            "order_id_str": str
-                        },
-                        "sl_order": {
-                            "order_id": int,
-                            "order_id_str": str
-                        }
-                    },
-                    "ts": int
-                }
-                Schema(schema).validate(res)
+            contract_type = get_contract_type(symbol_period)
+            lastprice = ATP.get_current_price()
+            tp_price = ATP.get_adjust_price(rate=1.05)
+            sl_price = ATP.get_adjust_price(rate=0.95)
+
+            buy_order = contract_api.contract_order(
+                symbol=symbol, contract_type=contract_type, price=lastprice, volume=1, direction="buy", offset="open")
+            pprint(buy_order)
+            sell_order = contract_api.contract_order(
+                symbol=symbol, contract_type=contract_type, price=lastprice, volume=1, direction="sell", offset="open")
+            pprint(sell_order)
+            time.sleep(2)
+
+            res = contract_api.contract_tpsl_order(symbol=symbol,
+                                                   contract_type=contract_type,
+                                                   direction="sell",
+                                                   volume=1,
+                                                   tp_trigger_price=tp_price,
+                                                   tp_order_price=tp_price,
+                                                   tp_order_price_type="limit",
+                                                   sl_trigger_price=sl_price,
+                                                   sl_order_price=sl_price,
+                                                   sl_order_price_type="limit")
+            time.sleep(1)
+            pprint(res)
+
+            schema = {"data": {"tp_order": {"order_id": int,
+                                            "order_id_str": str},
+                               "sl_order": {"order_id": int,
+                                            "order_id_str": str}
+                               },
+                      "status": "ok",
+                      "ts": int}
+            Schema(schema).validate(res)
 
     @allure.step('恢复环境')
     def teardown(self):
         print('\n恢复环境操作')
-        print(ATP.clean_market())
+        print(ATP.cancel_all_tpsl_order())
         print(ATP.cancel_all_order())
+        print(ATP.close_all_position())
 
 
 if __name__ == '__main__':
--- testCase/ContractTestCase/18_API/Schema/TestApiSchema_056.py
+++ testCase/ContractTestCase/18_API/Schema/TestApiSchema_056.py
@@ -18,13 +18,15 @@
     1
 """
 
+import time
+from pprint import pprint
 
 import allure
-import common.util
 import pytest
-from common.ContractServiceAPI import common_user_contract_service_api as common_contract_api
+from schema import Schema
+
 from common.ContractServiceAPI import t as contract_api
-from schema import Or, Schema
+from common.util import get_contract_type
 from tool.atp import ATP
 
 
@@ -39,39 +41,55 @@ class TestApiSchema_056:
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, symbol):
         print("前置条件 {}".format(symbol))
-        print(ATP.make_market_depth())
 
     @allure.title('止盈止损订单全部撤单')
     @allure.step('测试执行')
-    def test_execute(self, sub_uid):
+    def test_execute(self, symbol, symbol_period):
         with allure.step('1、调用接口：api/v1/contract_tpsl_cancelall'):
             pass
         with allure.step('2、接口返回的json格式、字段名、字段值正确'):
             # 构造持仓量
-            price = ATP.get_current_price()
-            common_contract_api.contract_order(
-                symbol="BTC", contract_type="this_week", price=price, volume=5, direction="sell", offset="open")
-            res_buy = contract_api.contract_trigger_order(
-                symbol="BTC", contract_type="this_week", trigger_type="ge", trigger_price=price, order_price=price-1, volume=1, direction="buy", offset="open", lever_rate=5)
-            print(res_buy)
-            res = contract_api.contract_tpsl_cancelall(symbol="BTC")
-            print(res)
-            if res["status"] != "error":
-                schema = {
-                    "status": "ok",
-                    "data": {
-                        "errors": [],
-                        "successes": str
-                    },
-                    "ts": int
-                }
-                Schema(schema).validate(res)
+            contract_type = get_contract_type(symbol_period)
+            lastprice = ATP.get_current_price()
+            tp_price = ATP.get_adjust_price(rate=1.05)
+            sl_price = ATP.get_adjust_price(rate=0.95)
+
+            buy_order = contract_api.contract_order(
+                symbol=symbol, contract_type=contract_type, price=lastprice, volume=1, direction="buy", offset="open")
+            pprint(buy_order)
+            sell_order = contract_api.contract_order(
+                symbol=symbol, contract_type=contract_type, price=lastprice, volume=1, direction="sell", offset="open")
+            pprint(sell_order)
+            time.sleep(2)
+
+            a = contract_api.contract_tpsl_order(symbol=symbol,
+                                                 contract_type=contract_type,
+                                                 direction="sell",
+                                                 volume=1,
+                                                 tp_trigger_price=tp_price,
+                                                 tp_order_price=tp_price,
+                                                 tp_order_price_type="limit",
+                                                 sl_trigger_price=sl_price,
+                                                 sl_order_price=sl_price,
+                                                 sl_order_price_type="limit")
+            time.sleep(1)
+            pprint(a)
+            res = contract_api.contract_tpsl_cancelall(symbol=symbol)
+            pprint(res)
+
+            schema = {"data": {"errors": [],
+                               "successes": str},
+                      "status": "ok",
+                      "ts": int}
+
+            Schema(schema).validate(res)
 
     @allure.step('恢复环境')
     def teardown(self):
         print('\n恢复环境操作')
-        print(ATP.clean_market())
+        print(ATP.cancel_all_tpsl_order())
         print(ATP.cancel_all_order())
+        print(ATP.close_all_position())
 
 
 if __name__ == '__main__':
